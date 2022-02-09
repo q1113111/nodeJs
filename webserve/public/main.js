7e227a7639a789056e7d4236da509931d50903ec
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2022-02-09 19:03:16 +0800

    呈現畫面

--- webserve/public/main.js
+++ webserve/public/main.js
@@ -1,11 +1,17 @@
 const search = document.querySelector('input')
 const btn = document.querySelector('button')
+const message = document.querySelector('#message')
 btn.addEventListener('click', async () => {
     const local = encodeURI(search.value)
     try {
         const data = await (await fetch(`http://localhost:3000/weather?city=${local}`)).json()
         console.log(data)
+        if(data.error){
+           return  message.textContent=data.error
+        }
+        message.textContent=data.datasetDescription
     } catch (error) {
         console.log(error)
+        message.textContent=error.error
     }
 })
