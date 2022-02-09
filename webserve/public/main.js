1510c70e266b51c14b5ff415c3d95b19223ba076
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2022-02-09 18:55:28 +0800

    前後端響應

--- webserve/public/main.js
+++ webserve/public/main.js
@@ -1,5 +1,11 @@
-console.log('hello index')
-
-const add = (a,b) =>{
-    return a+b
-}
+const search = document.querySelector('input')
+const btn = document.querySelector('button')
+btn.addEventListener('click', async () => {
+    const local = encodeURI(search.value)
+    try {
+        const data = await (await fetch(`http://localhost:3000/weather?city=${local}`)).json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+})
--- webserve/src/app.js
+++ webserve/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const hbs = require('hbs')
+const { getWeather } = require('./utils')
 //設置首頁資訊 
 // app.get('/',(req,res)=>{
 //     res.send('hello express')
@@ -11,57 +12,66 @@ const hbs = require('hbs')
 
 
 //hbs模板設置
-const viewPath = path.join(__dirname,'../template/views')
-const partialsPath = path.join(__dirname,'../template/partials')
-app.set('view engine','hbs')
-app.set('views',viewPath)
+const viewPath = path.join(__dirname, '../template/views')
+const partialsPath = path.join(__dirname, '../template/partials')
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
 //使用header.hbs
-hbs.registerPartials(partialsPath, function (err) {});
+hbs.registerPartials(partialsPath, function (err) { });
 
-app.get('',(req,res)=>{
-    res.render('index',{
-        author:'Sam',
-        content:'Hello hbs'
+app.get('', (req, res) => {
+    res.render('index', {
+        author: 'Sam',
+        content: 'Hello hbs'
     })
 })
 
-app.get('/help',(req,res)=>{
-    res.render('help',{
-        author:'Sam',
-        content:'Hello hbs'
+// 靜態頁面路徑設定
+const pathPublic = path.join(__dirname, '../public')
+app.use(express.static(pathPublic))
+// 控制網址傳入參數
+app.get('/weather', (req, res) => {
+    const city = req.query.city
+    if(!city){
+        return res.send({
+            error:'請填入縣市'
+        })
+    }
+    getWeather(city,(data)=>{
+        //JSON格式
+        res.send(data)
+    })
+
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        author: 'Sam',
+        content: 'Hello hbs'
     })
 })
 
 //help 子頁 not find 
-app.get('/help/*',(req,res)=>{
-    res.render('404',{
-        author:'Sam',
-        content:'Hello hbs',
-        errorMessage:'help subs not find page'
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        author: 'Sam',
+        content: 'Hello hbs',
+        errorMessage: 'help subs not find page'
     })
 })
 
 
 //404 not find 
-app.get('*',(req,res)=>{
-    res.render('404',{
-        author:'Sam',
-        content:'Hello hbs',
-        errorMessage:'not find page'
+app.get('*', (req, res) => {
+    res.render('404', {
+        author: 'Sam',
+        content: 'Hello hbs',
+        errorMessage: 'not find page'
     })
 })
 
 
 
 
-// help.html路徑設定
-const pathPublic = path.join(__dirname,'../public')
-app.use(express.static(pathPublic))
-app.get('/weather',(req,res)=>{
-    //JSON格式
-    res.send({
-        template:'30度',
-        location:'台中市'
-    })
-})
+
 app.listen(3000)
