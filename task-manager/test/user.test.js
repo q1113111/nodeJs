613256271977a4bfcfb527cddaca1e5c9aa9c70e
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2022-10-03 14:19:42 +0800

    feat: task test

--- task-manager/test/user.test.js
+++ task-manager/test/user.test.js
@@ -1,9 +1,8 @@
 const request = require('supertest')
 const app = require('../src/app')
 const { User } = require('../src/model/user')
-const jwt = require('jsonwebtoken')
-const mongoose = require('mongoose')
 
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
 const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
     _id: userOneId,
@@ -16,10 +15,7 @@ const userOne = {
 }
 
 //執行測試前 刪除所有資料
-beforeEach(async () => {
-    await User.deleteMany()
-    await new User(userOne).save()
-})
+beforeEach(setupDatabase)
 
 test('sing up a new user', async () => {
   const response =   await request(app).post('/user').send({
@@ -76,7 +72,39 @@ test('Should not delete account for unauthenticate user', async () => {
         .expect(401)
 })
 
-test('sho')
+test('Should upload avatar image', async() => {
+    // attach 圖片上傳
+    await request(app)
+        .post('/user/me/avatar')
+        .set('Authorization', userOne.tokens[0].token)
+        .attach('avatar', 'test/fixtures/view.jpeg')
+        .expect(200)
+    const user = await User.findById(userOneId)
+    // toEqual 比較物件類型 Buffer類型
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async() => {
+    await request(app)
+        .patch('/user/me')
+        .set('Authorization', userOne.tokens[0].token)
+        .send({
+            name: 'Jess'
+        })
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.name).toEqual('Jess')
+})
+
+test('Should not update invalid user fields', async() => {
+    await request(app)
+        .patch('/user/me')
+        .set('Authorization', userOne.tokens[0].token)
+        .send({
+            location: 'Philadelphia'
+        })
+        .expect(400)
+})
 //執行測試後
 afterEach(async () => {
     // console.log('after is done')
